8cb1d5d84316054a5da31d94acf30fd4172515c8
codedusting <codedusting@gmail.com>
2023-09-29 22:13:53 +0530

    Initial Setup with prettier done

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import "../styles/globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+
+export const metadata: Metadata = {
+  title: "Next Crash Course",
+  description: "This is a NextJS 13 App Router Crash Course for beginners",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.variable}`}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <h1 className="p-2 px-2 text-4xl font-bold">
+      NextJS 13 App Router Crash Course
+    </h1>
+  );
+};
+
+export default Home;
